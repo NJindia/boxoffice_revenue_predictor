728ee07ea2fe1f4b6a9cc7e2d3e45d724ccb64c6
Nikhil Jindia <nikhiljindia@gmail.com>
2021-11-17 15:52:40 -0500

    platform change

--- models/__init__.py
+++ models/__init__.py
@@ -10,6 +10,7 @@ from sklearn import metrics
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV
 
 if __name__ == '__main__':
     SPLIT_RATIO = .6
@@ -47,10 +48,30 @@ if __name__ == '__main__':
     split_idx = int(len(X) * SPLIT_RATIO)
 
 
-    def get_train_test(X):
+    def _get_train_test(X):
         X_train = X.iloc[0:split_idx]
         X_test = X.iloc[split_idx:, ]
         return X_train, X_test
+
+
+    def _get_cv_iterator(X, n_folds):
+        X_train_shuffled = X.iloc[:split_idx].sample(frac=1).reset_index(drop=True)
+        X_test_shuffled = X.iloc[split_idx:, ].sample(frac=1).reset_index(drop=True)
+        test_split_len = math.ceil(len(X_test_shuffled) / n_folds)
+        train_split_len = math.ceil(len(X_train_shuffled) / n_folds)
+        X_test_splits = []
+        X_train_splits = []
+        for i in range(n_folds - 1):
+            test_split = X_test_shuffled.iloc[test_split_len * i:test_split_len * (i + 1)]
+            X_test_splits.append(test_split)
+            train_split = X_train_shuffled.iloc[train_split_len * i:train_split_len * (i + 1)]
+            X_train_splits.append(train_split)
+        X_test_splits.append(X_test_shuffled.iloc[test_split_len * (n_folds - 1):])
+        X_train_splits.append(X_train_shuffled.iloc[train_split_len * (n_folds - 1):])
+
+        iterator = []
+        for train_split, test_split in zip(X_train_splits, X_test_splits):
+            iterator.append(zip(train_split, test_split))
 
 
     # eigen(X)
@@ -80,10 +101,10 @@ if __name__ == '__main__':
 
 
     # TODO: ONLY PREDICT ON LATEST MOVIES, POWER IS NOT GOOD FOR PREDICTING OLDER MOVIES
-
+    _get_cv_iterator(X, 4)
     # Base test
     regr = linear_model.LinearRegression()
-    X_train, X_test = get_train_test(X)
+    X_train, X_test = _get_train_test(X)
     print('Lin Reg Train Baseline')
     test_model(regr, X_train, y_train, X_train, y_train)
 
@@ -91,48 +112,51 @@ if __name__ == '__main__':
     test_model(regr, X_train, y_train, X_test, y_test)
 
     print('Lin Reg No MPAA')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(
         X.drop(columns=['mpaa_g', 'mpaa_pg', 'mpaa_pg-13', 'mpaa_r', 'mpaa_nc-17', 'mpaa_unrated']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Genres')
     genre_cols = [col_name for col_name in X if 'genre_' in col_name]
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=genre_cols))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=genre_cols))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Distributors')
     distributor_cols = [col_name for col_name in X if 'distributor_' in col_name]
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=distributor_cols))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=distributor_cols))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Opening Theatres')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=['opening_theaters']))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=['opening_theaters']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Runtime')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=['runtime_minutes']))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=['runtime_minutes']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Release Day')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=['release_day']))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=['release_day']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Release Month')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=['release_month']))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=['release_month']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     print('Lin Reg No Release Year')
-    X_feature_eval_train, X_feature_eval_test = get_train_test(X.drop(columns=['release_year']))
+    X_feature_eval_train, X_feature_eval_test = _get_train_test(X.drop(columns=['release_year']))
     test_model(regr, X_feature_eval_train, y_train, X_feature_eval_test, y_test)
 
     # X_sum = X.drop(columns=['director_power_p'])
     # X_power = X.drop(columns=['director_power_s'])
     # test_model(regr, X_sum, y_opening)
     # test_model(regr, X_power, y_opening)
-    kernel = 'rbf'
-    c = 1.0
-    eps = .2
-    svr = SVR(kernel=kernel, C=c, epsilon=eps)
+
+    params = [
+        {"kernel": ["rbf"], "gamma": np.logspace(-9, 9, 19), "C": np.logspace(-9, 9, 19), "epsilon": range(0.1, 2, .1)}
+    ]
+
+    grid_reg = GridSearchCV(SVR(), params, n_jobs=-1, cv=cv)
+    grid_reg.fit(X_train, y_train)
     print('SVR')
     test_model(svr, X_train, y_train, X_test, y_test)
     # test_model(svr, X_sum, y_opening)
